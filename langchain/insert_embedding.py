a428311f8050c572959bb7959996d984dd95b3be
norazhou <lhzhou1991@gmail.com>
2023-08-24 19:11:37 +0800

    Update insert_embedding.py

--- langchain/insert_embedding.py
+++ langchain/insert_embedding.py
@@ -12,10 +12,10 @@ def insert_vector(data):
     vendor_id = None
     try:
         # connect to the PostgreSQL database
-        conn = psycopg2.connect(host="database-1.cb1rukuxcy0o.us-east-1.rds.amazonaws.com", 
-                                database="docembedding",
-                                user="postgres",
-                                password="password")
+        conn = psycopg2.connect(host=DB_HOST, 
+                                database=DB_NAME,
+                                user=USER,
+                                password=PASSWORD)
         # create a new cursor
         cur = conn.cursor()
         # execute the INSERT statement
